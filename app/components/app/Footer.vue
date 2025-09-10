e9a655faaaaf87117f1f9df06b1bee34c9670044
Калмыков Денис Владимирович <23201071@live.preco.ru>
2025-09-10 18:13:13 +0500

    ui

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-let idActiveButtons = -1; 
+let idActiveButton = -1; 
+
+const checkID = (id:number):Boolean => id === idActiveButton
+const setidActiveButton = (id:number):void => {
+  idActiveButton = id
+}
+
 </script>
 
 <template>
   <footer bg-brand-primary h-23 rounded-t-10 absolute w-full>
     <nav flex justify-center gap-5 items-center>
-      <FooterButton src-img="/img/footer/moneybag.svg" title-button="Магазин" :is-active="idActiveButtons == 0 ? true: false" @sumbit="()=>{idActiveButtons = 0}"/>
-      <FooterButton src-img="/img/footer/globe.svg" title-button="Карта мира" :is-active="idActiveButtons == 1 ? true: false" @sumbit="()=>{idActiveButtons = 1}" />
-      <FooterButton src-img="/img/footer/user.svg" title-button="Аккаунт" :is-active="idActiveButtons == 2 ? true: false" @sumbit="()=>{idActiveButtons = 2}"/>
+      <FooterButton src-img="/img/footer/moneybag.svg" title-button="Магазин" :is-active="checkID(0)" @sumbit="setidActiveButton(0)"/>
+      <FooterButton src-img="/img/footer/globe.svg" title-button="Карта мира" :is-active="checkID(1)" @sumbit="setidActiveButton(1)" />
+      <FooterButton src-img="/img/footer/user.svg" title-button="Аккаунт" :is-active="checkID(2)" @sumbit="setidActiveButton(2)"/>
     </nav>
   </footer>
 </template>
