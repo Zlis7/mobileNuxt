195991476cf95945bced97dd884c318cdfa50b5f
Калмыков Денис Владимирович <23201071@live.preco.ru>
2025-09-11 16:55:43 +0500

    ui

---
+++ app/pages/basket/index.vue
@@ -0,0 +1,69 @@
+<script setup lang=ts>
+definePageMeta({
+  title: 'Корзина',
+})
+</script>
+
+<template>
+  <div bg-brand-primary border-18 border-brand-primary flex flex-col gap-2 rounded-10 mb-2>
+    <div bg-brand rounded-5 p-5>
+      <div flex flex-col  gap-5 items-center h-168 wrap-normal overflow-x-scroll>
+    
+        <div bg-brand-primary w-70 h-30 flex gap-2  items-center  pr-3 rounded-4xl>
+           <div pl-3 flex flex-col gap-3>
+            <div flex gap-2 items-center>
+                <NuxtImg w-7 h-7 src="/img/main/badge-dollar.svg" />
+                <p text-5>7800</p>
+            </div>
+            <p overflow-hidden text-ellipsis h-10 text-3>УУием ение макси ение максиммакс маение максиммаксмаммаксмас прав атимальных прав атакиальных</p>
+
+           </div>
+            <NuxtImg w-8 h-8 src="/img/main/basket-shopping.svg" />
+
+        </div>
+
+        <div bg-brand-primary w-70 h-30 flex gap-2  items-center  pr-3 rounded-4xl>
+           <div pl-3 flex flex-col gap-3>
+            <div flex gap-2 items-center>
+                <NuxtImg w-7 h-7 src="/img/main/badge-dollar.svg" />
+                <p text-5>7800</p>
+            </div>
+            <p overflow-hidden text-ellipsis h-10 text-3>УУием ение макси ение максиммакс маение максиммаксмаммаксмас прав атимальных прав атакиальных</p>
+
+           </div>
+            <NuxtImg w-8 h-8 src="/img/main/basket-shopping.svg" />
+
+        </div>
+
+        <div bg-brand-primary w-70 h-30 flex gap-2  items-center  pr-3 rounded-4xl>
+           <div pl-3 flex flex-col gap-3>
+            <div flex gap-2 items-center>
+                <NuxtImg w-7 h-7 src="/img/main/badge-dollar.svg" />
+                <p text-5>7800</p>
+            </div>
+            <p overflow-hidden text-ellipsis h-10 text-3>УУием ение макси ение максиммакс маение максиммаксмаммаксмас прав атимальных прав атакиальных</p>
+
+           </div>
+            <NuxtImg w-8 h-8 src="/img/main/basket-shopping.svg" />
+
+        </div>
+        <div bg-brand-primary w-70 h-30 flex gap-2  items-center  pr-3 rounded-4xl>
+           <div pl-3 flex flex-col gap-3>
+            <div flex gap-2 items-center>
+                <NuxtImg w-7 h-7 src="/img/main/badge-dollar.svg" />
+                <p text-5>7800</p>
+            </div>
+            <p overflow-hidden text-ellipsis h-10 text-3>УУием ение макси ение максиммакс маение максиммаксмаммаксмас прав атимальных прав атакиальных</p>
+
+           </div>
+            <NuxtImg w-8 h-8 src="/img/main/basket-shopping.svg" />
+
+        </div>
+
+    
+
+       
+      </div>
+    </div>
+  </div>
+</template>
